dda42a1e6eab30292dfd14761fa1e32eec79a664
nikkk1912 <moskalevskiy1@gmail.com>
2024-02-03 23:55:11 +0200

    added pictures to embeds

--- commands/utility/weather.js
+++ commands/utility/weather.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder } = require("discord.js");
 const { fetchWeatherApi } = require('openmeteo');
 require("dotenv").config();
 
@@ -13,16 +13,14 @@ module.exports = {
         .setDescription("Where to check weather")
         .setRequired(true)
     ),
+
   async execute(interaction) {
     let city = interaction.options.getString("city");
     let cityForPrint = city[0].toUpperCase() + city.slice(1).toLowerCase();
-
     let data;
 
-
+    //Geocode API call
     const apiUrl = `https://geocode.maps.co/search?city=${city}&api_key=${process.env.GEOCODING_KEY}`;
-
-    //API CALL
     fetch(apiUrl)
       .then((response) => {
         if (!response.ok) {
@@ -33,64 +31,85 @@ module.exports = {
       })
       .then((responseData) => {
         data = responseData;
+
+        //Open-meteo API call
+        //parameters for api call
         const params = {
-            "latitude": data[0].lat,
-            "longitude": data[0].lon,
-            "current": "temperature_2m",
-            "hourly": "temperature_2m",
-            "daily": ["temperature_2m_max", "temperature_2m_min"],
-            "timezone": "Europe/Moscow",
-            "forecast_days": 1
-          };
-          
-          const url = "https://api.open-meteo.com/v1/forecast";
-          
-          fetchWeatherApi(url, params)
-            .then((responses) => {
-              const response = responses[0];
-              const utcOffsetSeconds = response.utcOffsetSeconds();
-              const current = response.current();
-          
-              const weatherData = {
-                current: {
-                  time: new Date((Number(current.time()) + utcOffsetSeconds) * 1000),
-                  temperature2m: current.variables(0).value(),
-                }
-              };
-      
-              const currentTemperature = Math.round(weatherData.current.temperature2m.toFixed(1));
+          "latitude": data[0].lat,
+          "longitude": data[0].lon,
+          "current": "temperature_2m",
+          "hourly": "temperature_2m",
+          "daily": ["temperature_2m_max", "temperature_2m_min"],
+          "timezone": "Europe/Moscow",
+          "forecast_days": 1
+        };
 
-              const temperatureEmbed = new EmbedBuilder().setTitle(`Temperature in city ${cityForPrint} is ${currentTemperature}°C`);
+        const url = "https://api.open-meteo.com/v1/forecast";
+        fetchWeatherApi(url, params)
+          .then((responses) => {
+            const response = responses[0];
+            const utcOffsetSeconds = response.utcOffsetSeconds();
+            const current = response.current();
 
-              if (currentTemperature <= -20) {
-                temperatureEmbed.setColor("Purple");
-              }else if (currentTemperature<= -5 && currentTemperature > -20) {
-                temperatureEmbed.setColor("Blue");
-              } else if (currentTemperature <= 5 && currentTemperature > -5) {
-                temperatureEmbed.setColor("White");
-              } else if (currentTemperature <=20 && currentTemperature > 5) {
-                temperatureEmbed.setColor("Yellow");
-              } else if (currentTemperature > 20) {
-                temperatureEmbed.setColor("Red");
+            const weatherData = {
+              current: {
+                time: new Date((Number(current.time()) + utcOffsetSeconds) * 1000),
+                temperature2m: current.variables(0).value(),
               }
+            };
+            const currentTemperature = Math.round(weatherData.current.temperature2m.toFixed(1));
 
 
-              console.log(currentTemperature);
-              interaction.reply({embeds: [temperatureEmbed] });
-          
-            })
-            .catch((error) => {
-              console.error('Error fetching weather data:', error);
-              interaction.reply('Error fetching weather data:', error);
-            });
+            //Creating embed for discord message
+            const temperatureEmbed = new EmbedBuilder().setTitle(`Temperature in city ${cityForPrint} is ${currentTemperature}°C`);
+            let picture;
 
-        //interaction.reply(`City: ${city}, long: ${data[0].lon}, lat: ${data[0].lat}`);
+            //Changing embed color and picture according to temp
+            if (currentTemperature <= -20) 
+            {
+              temperatureEmbed.setColor("Purple");
+              picture = new AttachmentBuilder('img/cold.png');
+              temperatureEmbed.setThumbnail('attachment://cold.png');
+            } 
+            else if (currentTemperature <= -5 && currentTemperature > -20) 
+            {
+              temperatureEmbed.setColor("Blue");
+              picture = new AttachmentBuilder('img/cold.png');
+              temperatureEmbed.setThumbnail('attachment://cold.png')
+            } 
+            else if (currentTemperature <= 5 && currentTemperature > -5) 
+            {
+              temperatureEmbed.setColor("White");
+              picture = new AttachmentBuilder('img/normal.png');
+              temperatureEmbed.setThumbnail('attachment://normal.png');
+            } 
+            else if (currentTemperature <= 20 && currentTemperature > 5) 
+            {
+              temperatureEmbed.setColor("Yellow");
+              picture = new AttachmentBuilder('img/hot.png');
+              temperatureEmbed.setThumbnail('attachment://hot.png');
+            } 
+            else if (currentTemperature > 20) 
+            {
+              temperatureEmbed.setColor("Red");
+              picture = new AttachmentBuilder('img/hot.png');
+              temperatureEmbed.setThumbnail('attachment://hot.png');
+            }
+
+            //Sending reply to server
+            console.log(currentTemperature);
+            interaction.reply({ embeds: [temperatureEmbed], files: [picture] });
+
+          })
+          .catch((error) => {
+            console.error('Error fetching weather data:', error);
+            interaction.reply('Error fetching weather data:', error);
+          });
+
       })
       .catch((error) => {
         console.error("Error1:", error);
         interaction.reply("Invalid city name");
       });
-    //-------------
-
   },
 };
